74c30414d11b5fb05267f8dd35a4233539c23f45
Darmen96 <67601251+Darmen96@users.noreply.github.com>
2021-02-14 21:00:26 +0600

    Creating components Child components ,data binding, adding data to existing table. I use ngFor for iteratively show data in result.component.

---
+++ src/app/model/recordDate.ts
@@ -0,0 +1,11 @@
+export class RecordDate {
+  jogDate: Date;
+  jogDistance: number;
+  jogTime: number;
+
+  constructor(jogDate: Date, jogDistance: number, jogTime: number) {
+    this.jogDate = jogDate;
+    this.jogDistance = jogDistance;
+    this.jogTime = jogTime;
+  }
+}
